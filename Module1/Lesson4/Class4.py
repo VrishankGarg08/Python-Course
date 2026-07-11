dfbd62f43430c7a90bcab7a5f4fe6b7f68f96a2a
VrishankGarg08 <vrishankgarg08@gmail.com>
2026-07-11 15:01:41 +0530

    lesson4

---
+++ Module1/Lesson4/Class4.py
@@ -0,0 +1,55 @@
+#Operators are symbols that performs specific mathematical,logical or assignement operations on values called operands
+
+# Operands are the values or variables on which operators act.
+
+# Types of Operators:
+#  1. Arithmetic Operators : These operators performs Mathematical calculations such as addition, subtraction, division and multiplication and more. Like: Add(+),Subtract(-),Multiply(*),Divide(/),Modulus(%),Floor division(//),Exponents(**).
+
+# Example of Arthimethic Operations:
+# a=25
+# b=15
+
+# print("Adding a & b :",a+b)
+# print("Subtract a & b :",a-b)
+# print("Product of a & b :",a*b)
+# print("Dividing a & b :",4/b) # It returns us with the quotient, fraction value or decimal value, which it will be dividing it fully.
+# print("Modulus of a & b :",a%b) # It returns us with the remainder
+# print("Floor division of a & b :",4//b) # It returns us with the Quotient 
+# print("a raised to the power of b :",a**b) # It raises the power.
+
+#  2. Comparison Operators : They are used to compare two operands and return either True or False, they are used for making decisions. like - Equal to(==),Not equal to(!=),greater than(>),Smaller than(<),Greater than equal to(>=),Less than equal to(<=).
+
+# Example of Comparison Operations:
+
+# total=560
+# last_year=500
+
+# print(total==last_year)
+# print(total!=last_year)
+# print(total<last_year)
+# print(total>last_year)
+# print(total<=last_year)
+# print(total>=last_year)
+
+#  3. Assignment Operators : They assign values to variables or update the current value of variables with new values often combining arithmetic or bitwise operations. Like: simple assignment(=), assign & add (+=), subtact and assign(-=), multiply and assign(*=), divide and assign (/=), modulus and assign (%=),floor division and assign(//=) , exponent and assign(**=).
+
+# Example of Assignment Operators:
+total=560
+# If the operands is in decimal for % & // , they will return value in decimal.
+total+=30
+print(total)
+total-=30
+print(total)
+total*=30
+print(total)
+total/=30
+print(total)
+total%=30
+print(total)
+total//=30
+print(total)
+total**=30
+print(total)
+
+
+# 4. Logical operators: Logical operators are used for logical operations such as and, or & not.
