d79c7a6db12934806ad67622ce50104004b6ee38
VrishankGarg08 <vrishankgarg08@gmail.com>
2026-07-19 20:02:25 +0530

    Lesson7

--- Module1/Lesson7/Class7.py
+++ Module1/Lesson7/Class7.py
@@ -12,5 +12,26 @@
 # print(a is c)
 # print(a is not b)
 
-# 2. Membership Operators :
-# 3. Bitwise Operators : 
+# 2. Membership Operators : These operators check whether a value exists within a sequence (list, string, tuple, set, dictionary) or not.
+# Two type of operators:
+# in : Returns True if found in the sequence.
+# not in: Returns True if not found in the sequence.
+
+# fruits=["apple","banana","mango"]
+# print("banana" in fruits)
+# print("grape" not in fruits)
+
+# city="Lucknow"
+
+# print("know" not in city)
+
+# student={"Name":"Ravi","Age":12}
+# print("Name" in student)
+# print("Riya" in student)
+
+# 3. Bitwise Operators : Bitwise operators work on binary representation of integers, manipulating individual bits.
+# Type: AND(&), OR(|), NOT(~)
+
+print(5&3) # 5=0101 & 3=0011 = + = 0001
+print(5|3) # 
+print(~3)  # 
