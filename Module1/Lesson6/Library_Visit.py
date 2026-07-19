236d8e670a4f9d826474490b6671e831c198ad88
VrishankGarg08 <vrishankgarg08@gmail.com>
2026-07-19 19:37:00 +0530

    Lesson7

--- Module1/Lesson6/Library_Visit.py
+++ Module1/Lesson6/Library_Visit.py
@@ -1,4 +1,8 @@
 print("Library Visit Planner")
 print("Answer 3 Questions and I will plan your Library visit!")
 print("")
-day=input("What Day is It?(MONDAY TO SUNDAY) :")
+day=input("What Day is It?(MONDAY TO SUNDAY) :").upper()
+weather=input("What Weather is It?(SUNNY/RAINY/CLOUDY) :").upper()
+book=input("Is your book returned?(Yes/No)").upper()
+if day=="MONDAY" or day=="TUESDAY" or day=="WEDNESDAY":
+    print
---
+++ Module1/Lesson7/Class7.py
@@ -0,0 +1,16 @@
+#Topics to be covered : 
+# 1. Identity Operators : They checks if two variables point to the same object or not. It has is and is not as the operators.
+# Two identity operators: 
+# is : Returns True if both variables point to the same object in the memory.
+# is not : Returns True if both variables point to the different object in the memory.
+# Example:
+# a=[1,2,3]
+# b=[1,2,3]
+# c=a
+# print(a==b)
+# print(a is b)
+# print(a is c)
+# print(a is not b)
+
+# 2. Membership Operators :
+# 3. Bitwise Operators : 
