036591adcd685451b26fce963d21e7349b927b4c
VrishankGarg08 <vrishankgarg08@gmail.com>
2026-08-16 16:44:57 +0530

    M2L5

---
+++ Module2/Lesson5/LoopArtDesigner.py
@@ -0,0 +1,50 @@
+Rows = int(input("Enter number of rows for star pattern: "))
+ 
+for i in range(Rows):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+
+
+print("FLOYD'S TRIANGLE")
+FloydRow= int(input("Enter number of rows for Floyd's Triangle: "))
+number = 1
+ 
+for i in range(1,FloydRow + 1):
+    for j in range(1, i + 1):
+        print(number, end=" ")
+        number += 1
+    print()
+print("DIAMOND NUMBER PATTERN")
+Row_size = int(input("Enter number of rows for diamond pattern: "))
+ 
+if Row_size % 2 == 0:
+    half_rows = Row_size // 2
+else:
+    half_rows = Row_size // 2 + 1
+
+space = half_rows - 1
+
+for i in range(1, half_rows + 1):
+    for j in range(1, space + 1):
+        print(" ", end="")
+    space -= 1
+    number = 1
+ 
+    for j in range(2 * i - 1):
+        print(number, end="")
+        number += 1
+    print()
+space = 1
+
+for i in range(1, half_rows):
+    for j in range(1, space + 1):
+        print(" ", end="")
+    space += 1
+    number = 1
+ 
+    for j in range(1, 2 * (half_rows - i)):
+        print(number, end="")
+        number += 1
+    print()
