2afe5c4ac2393934fbd842791ab2b65ad281740a
VrishankGarg08 <vrishankgarg08@gmail.com>
2026-08-22 14:27:07 +0530

    M2L7

---
+++ Module2/Lesson7/Class15.py
@@ -0,0 +1,12 @@
+# TOPIC -- FUNCTION
+# Function is a block of code containing related statements. that perform 1 specific task. Function's let you break a big program into smaller pieces of organised code which helps us from repeating the same line again and again through out the code.
+#Types of Function :
+# Built in : They are the functions that already exsist in PYTHON..
+# User Defined : They are the functions which are defined by US .
+def greet ():
+    return("Welcome")
+
+def name () :
+    print("Home")
+name () 
+greet ()
