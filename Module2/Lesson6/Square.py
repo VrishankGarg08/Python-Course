4e0df0af73f606af7dfbbeead041b23433dc80d9
VrishankGarg08 <vrishankgarg08@gmail.com>
2026-08-30 10:26:51 +0530

    M2l6

--- Module2/Lesson6/Square.py
+++ Module2/Lesson6/Square.py
@@ -1,15 +1,7 @@
 import turtle
-
-# Create the turtle
 t = turtle.Turtle()
-
-# Set the speed of the turtle
 t.speed(3)
-
-# Draw the square
 for i in range(4):
     t.forward(100)
     t.right(90)
-
-# Keep the window open
 turtle.done()
